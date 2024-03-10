d5a4c13acf580e43c9663c75ab1eb0e1c32904bc
djkhz <zheezs@hotmail.com>
2024-03-10 12:55:10 +0700

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "Running composer"
-#composer global require kreait/firebase-php:6.1.0
-composer global require "kreait/firebase-php:^7.0" 
-#composer install --no-dev --working-dir=/var/www/html
-composer install
+composer global require kreait/firebase-php:6.1.0
+#composer global require "kreait/firebase-php:^7.0" 
+composer install --no-dev --working-dir=/var/www/html
+#composer install
 conf/nginx/nginx-site.conf
 
 # #!/usr/bin/env bash
