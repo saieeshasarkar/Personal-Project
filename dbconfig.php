da30e1b82e6da046a2412790da06917c8ea8e18a
djkhz <zheezs@hotmail.com>
2024-03-10 11:28:51 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -10,12 +10,17 @@
 // ->withProjectId('my-project')
 // ->withDatabaseUri('https://authentication-php-default-rtdb.asia-southeast1.firebasedatabase.app/');
 
-
-    $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/dengue-fever-database-6da72-firebase-adminsdk-96c66-8cdfbb7728.json');
+ $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/test.json');
     $firebase=(new Factory)
         ->withServiceAccount($serviceAccount)
-        ->withDatabaseUri('https://dengue-fever-database-6da72-default-rtdb.asia-southeast1.firebasedatabase.app/')
+        ->withDatabaseUri('https://authentication-php-default-rtdb.asia-southeast1.firebasedatabase.app/')
         ->Create();
+
+    // $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/dengue-fever-database-6da72-firebase-adminsdk-96c66-8cdfbb7728.json');
+    // $firebase=(new Factory)
+    //     ->withServiceAccount($serviceAccount)
+    //     ->withDatabaseUri('https://dengue-fever-database-6da72-default-rtdb.asia-southeast1.firebasedatabase.app/')
+    //     ->Create();
     
     $database = $firebase->getDatabase();
 
