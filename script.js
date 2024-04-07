fbfa695dfa7d51cdad66ca2732dc37b3b1bd4fd5
djkhz <zheezs@hotmail.com>
2024-04-08 00:29:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ var isMobile = false; //initiate as false
 		var district_lay = new L.GeoJSON.AJAX("data/district_pov.geojson",{onEachFeature:popUp, style:styleD});
 		var province_lay = new L.GeoJSON.AJAX("data/province_pov.geojson",{onEachFeature:popUp,style:styleP}).addTo(m);
 		//var district_lay = new L.GeoJSON.AJAX("https://data.opendevelopmentmekong.net/lo/dataset/0073f53b-4852-4463-ba8d-32bdef6f5476/resource/d6156852-a57e-4908-8db4-768d9efcad21/download/district_pov.geojson",{onEachFeature:popUp, style:styleD});
-		var district_point = new L.GeoJSON.AJAX("data/distrit_point.geojson", {
+		var district_point = new L.GeoJSON.AJAX("data/district_point.geojson", {
 	pointToLayer: function (feature, latlng) {
 	  var circleMarker = L.circle(latlng, {
 		radius: 0,
