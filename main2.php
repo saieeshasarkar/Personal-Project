dd8fc86d9e35bf74c1dd2f4b1756390576c7dc6a
djkhz <zheezs@hotmail.com>
2024-06-24 00:28:38 +0700

    Update main2.php

--- main2.php
+++ main2.php
@@ -3,6 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js"></script>
+	
+	<script type="text/javascript" src="scripts/leaflet.ajax.js"></script>
+	
+	<script src="scripts/spin.js"></script>
+	<script src="scripts/leaflet.spin.js"></script>
+	
+	
+	<script type="text/javascript" src="scripts/jquery.min.js"></script>
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.7.0/introjs.min.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.7.0/intro.min.js"></script>
+	
+	<link rel="stylesheet" href="style/leaflet.groupedlayercontrol.css" />
+	<script type="text/javascript" src="scripts/leaflet.groupedlayercontrol.js"></script>
+	
+	<script type="text/javascript" src="scripts/leaflet-geojson-selector.js"></script>
+		
+	<link rel="stylesheet" href="style/leaflet.zoomhome.css"/>
+	<script src="scripts/leaflet.zoomhome.min.js"></script>
+	
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="screen" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.css"/>
+	<script src="https://cdn.jsdelivr.net/npm/leaflet-easybutton@2/src/easy-button.js"></script>
+	
+	<link rel="stylesheet" href="StyleMap.css" />
     <title>Multi-page App - Bootstrap</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -73,10 +100,57 @@
         </div>
 
         <div id="map-page" class="page">
-            <h2>Map</h2>
+            <h2>Map</h2>\   
+<script>
+// Use the PHP variable in JavaScript
+let data = JSON.parse('<?php echo $jsonData; ?>');
+let result = {};
+// let counts = {};
+let counts = {total:0};
+data.forEach(item => {
+    let [key1, key2, value] = item.split("-");
+    if (!result[key1]) {
+        result[key1] = {};
+		counts[key1] = { total: 0 };
+    }
+    if (!result[key1][key2]) {
+        result[key1][key2] = [];
+		counts[key1][key2] = { total: 0, unique: {} };
+    }
+    if (!counts[key1][key2].unique[value]) {
+        counts[key1][key2].unique[value] = 0;
+    }
+    result[key1][key2].push(value);
+	counts[key1][key2].unique[value]++;
+    counts[key1][key2].total++;
+    counts[key1].total++;
+    counts.total++;
+});
+
+console.log("Counts:", counts);
+
+
+function countMembers(data, key, subKey) {
+    if (data.hasOwnProperty(key)) {
+        if (subKey && data[key].hasOwnProperty(subKey)) {
+            return data[key][subKey].length;
+        } else {
+            let count = 0;
+            for (let subKey in data[key]) {
+                count += data[key][subKey].length;
+            }
+            return count;
+        }
+    }
+    return 0;
+}
+
+console.log(result);
+console.log(countMembers(data, '1', '101'));  // Outputs: 2
+</script>
             <div id="map">
-            <?php include 'map.php';?>
             </div>
+            <script type="text/javascript" src="script.js"></script>
         </div>
 
         <div id="user" class="page">
