3d59db8332649b194b3e28a234cf4d4d179cac74
djkhz <zheezs@hotmail.com>
2024-08-11 12:59:15 +0700

    map

--- script.js
+++ script.js
@@ -404,8 +404,8 @@ var isMobile = false; //initiate as false
 				color: '#636363',
 				fillOpacity: 0.4
 			});
-			// province_point.bringToFront();
-			// district_point.bringToFront();
+			province_point.bringToFront();
+			district_point.bringToFront();
 			info.update(layer.feature.properties);
 		};
 
@@ -415,8 +415,8 @@ var isMobile = false; //initiate as false
 			// layer.setStyle(layer.options.previousStyle);
 			province_lay.setStyle(styleP);
 			district_lay.setStyle(styleD);
-			province_point.setStyle(styleV);
-			district_point.setStyle(styleV);
+			// province_point.setStyle(styleV);
+			// district_point.setStyle(styleV);
 			// village_lay.setStyle(styleV);
 			info.update();
 		};
