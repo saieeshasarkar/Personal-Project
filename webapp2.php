b1c8344d02f8b3c87a321ee14341ff01837a2afd
djkhz <zheezs@hotmail.com>
2024-12-08 03:09:11 +0700

    Update webapp2.php

--- webapp2.php
+++ webapp2.php
@@ -920,7 +920,7 @@ let autocompleteDatax = {};
                 // Combine the dates into a range
                 if (startDate && endDate) {
                   dateRangeInput.value = `${startDate.toISOString().split('T')[0]} to ${endDate.toISOString().split('T')[0]}`;
-		resetRealDB(allData,${startDate.toISOString().split('T')[0]},${endDate.toISOString().split('T')[0]});
+		resetRealDB(allData,`${startDate.toISOString().split('T')[0]}`,`${endDate.toISOString().split('T')[0]}`);
 		startDateSelected = false;
 		endDateSelected =false;
 		  // indicator.textContent = ''; // Clear the indicator after selection
