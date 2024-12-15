6963143758d5e2225a9fc4a85d6e63b0a1278698
djkhz <zheezs@hotmail.com>
2024-12-15 12:28:07 +0700

    Update scriptc.js

--- scriptc.js
+++ scriptc.js
@@ -441,14 +441,17 @@ function decompressGzip(gzipData) {
 
 		var breport = L.control({position: 'topright'});
 		breport.onAdd = function () {
-			var div = L.DomUtil.create('div', 'breport'),
+			var div = L.DomUtil.create('div', 'info');
+			div.style.width = '100px';
+			div.style.text-align = 'center';
+			div.style.padding = '10px';
 			//this is for adding a logo as needed
 			//holder ='<table><tr>' 
 			//logo = '<td rowspan=2><img class="logo" src="logo_laos.png"></img></td>';
 			//labels = "<td><h4>Poverty in Lao PDR</h4></td></tr><tr><td><p>Percentage of people in poverty by province/district: 2015</p></td></tr></table>";
 			labelsn = "<h4>Dangue in Lao PDR</h4><p>";//<div class='input-field col s12'><input type='text' id='autocomplete-inputx' class='autocomplete'><label for='autocomplete-inpuxt'>Search for a district or province</label></div>";//<p>Percentage of people in poverty by province/district: 2015</p>";
 			// var elemsx = document.querySelectorAll('#autocomplete-input');
-			btnr = '<button class="btn waves-effect waves-light" type="submit">View Report<i class="material-icons right">assessment</i></button>';
+			btnr = '<a href="#" class="modal-trigger" data-target="loginModal" id="loginLink"><i class="material-icons">assessment</i><br>View Report</a>';
 			div.innerHTML = btnr;//holder + logo + labels;
 
 			// elemsx.forEach(function(element) {
